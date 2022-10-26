049734c9a1446c945ef34aad54d78decdb184251
Yuyeon Oh <yuyeon.oh@samsung.com>
2022-10-26 16:19:32 +0900

    Rename application yml

--- src/main/java/com/kbtest/client/KakaoClient.java
+++ src/main/java/com/kbtest/client/KakaoClient.java
@@ -1,27 +1,32 @@
 package com.kbtest.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
-@RequiredArgsConstructor
+@Service
 public class KakaoClient {
     private final WebClient webClient;
 
     @Value("client.kakao.url")
-    private final String kakaoUrl;
+    private String kakaoUrl;
 
     @Value("client.kakao.uri")
-    private final String kakaoUri;
+    private String kakaoUri;
 
     @Value("client.kakao.key")
-    private final String kakaoKey;
+    private String kakaoKey;
+
+    public KakaoClient(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("dapi.kakao.com")
+                .defaultHeader("Authorization", "KakaoAK 283a7a18acec1b73460173469500bea3").build();
+    }
 
     public Mono<String> searchPlace(String keyword) {
-        return webClient.mutate().baseUrl(kakaoUrl).defaultHeader("Authorization", kakaoKey).build().get()
-                .uri(uriBuilder -> uriBuilder.path(kakaoUri).queryParam("query", keyword).build()).retrieve()
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/v2/local/search/address.json").queryParam("query", keyword).build()).retrieve()
                 .bodyToMono(String.class);
     }
 
