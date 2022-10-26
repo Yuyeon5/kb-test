1b3df522da80aca8ee74cb136eca68aad7a20446
Yuyeon Oh <yuyeon.oh@samsung.com>
2022-10-26 15:58:27 +0900

    Add kakao api for test

--- src/main/java/com/kbtest/controller/PlaceController.java
+++ src/main/java/com/kbtest/controller/PlaceController.java
@@ -4,10 +4,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kbtest.service.PlaceService;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @RestController
+@RequiredArgsConstructor
 public class PlaceController {
+
+    private final PlaceService service;
+
     @GetMapping("/v1/place")
     public void searchPlace(@RequestParam("q") String keyword) {
 
+    }
+
+    @GetMapping("/v1/kakao/place")
+    public Mono<String> testKakaoPlace(@RequestParam("q") String keyword) {
+        return service.searchKakaoPlace(keyword);
     }
 }
--- src/main/java/com/kbtest/service/PlaceService.java
+++ src/main/java/com/kbtest/service/PlaceService.java
@@ -2,7 +2,17 @@ package com.kbtest.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class PlaceService {
+import com.kbtest.client.KakaoClient;
 
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class PlaceService {
+    private final KakaoClient kakaoClient;
+
+    public Mono<String> searchKakaoPlace(String keyword) {
+        return kakaoClient.searchPlace(keyword);
+    }
 }
