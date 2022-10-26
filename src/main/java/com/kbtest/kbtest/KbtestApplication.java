8fcbdd52d07547f5f88946a6131974f297e6ff6d
Yuyeon Oh <yuyeon.oh@samsung.com>
2022-10-26 10:20:08 +0900

    Init spring boot with webflux

---
+++ src/main/java/com/kbtest/kbtest/KbtestApplication.java
@@ -0,0 +1,13 @@
+package com.kbtest.kbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbtest/kbtest/KbtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbtest.kbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
