8907f2c0b20012332e6c6eb929f6a6f5397ad306
Yuyeon Oh <u.yeon.o@gmail.com>
2022-10-27 01:02:43 +0900

    Divide long function

--- src/main/java/com/kbtest/service/PlaceService.java
+++ src/main/java/com/kbtest/service/PlaceService.java
@@ -28,6 +28,29 @@ public class PlaceService {
     private static final String KAKAO = "kakao";
     private static final String NAVER = "naver";
 
+    private int comparePlace(CommonPlace a, CommonPlace b) {
+        Map<String, Integer> aOrderMap = a.getOrderMap();
+        Map<String, Integer> bOrderMap = b.getOrderMap();
+
+        int numResultA = aOrderMap.size();
+        int numResultB = bOrderMap.size();
+        int diff = numResultB - numResultA;
+
+        if (diff != 0) {
+            return diff;
+        } else if (aOrderMap.containsKey(KAKAO) && bOrderMap.containsKey(KAKAO)) {
+            return aOrderMap.get(KAKAO) - bOrderMap.get(KAKAO);
+        } else if (aOrderMap.containsKey(NAVER) && bOrderMap.containsKey(NAVER)) {
+            return aOrderMap.get(NAVER) - bOrderMap.get(NAVER);
+        }
+
+        if (aOrderMap.containsKey(KAKAO)) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
     public Mono<CommonResponse> searchPlace(String keyword) {
         Mono<CommonResponse> kakaoResult = kakaoClient.searchPlace(keyword);
         Mono<CommonResponse> naverResult = naverClient.searchPlace(keyword);
@@ -73,29 +96,7 @@ public class PlaceService {
             log.info("merged place count - {}", placeMap.size());
 
             List<CommonPlace> mergedPlaces = new ArrayList<>(placeMap.values());
-
-            Collections.sort(mergedPlaces, (a, b) -> {
-                Map<String, Integer> aOrderMap = a.getOrderMap();
-                Map<String, Integer> bOrderMap = b.getOrderMap();
-
-                int numResultA = aOrderMap.size();
-                int numResultB = bOrderMap.size();
-                int diff = numResultB - numResultA;
-
-                if (diff != 0) {
-                    return diff;
-                } else if (aOrderMap.containsKey(KAKAO) && bOrderMap.containsKey(KAKAO)) {
-                    return aOrderMap.get(KAKAO) - bOrderMap.get(KAKAO);
-                } else if (aOrderMap.containsKey(NAVER) && bOrderMap.containsKey(NAVER)) {
-                    return aOrderMap.get(NAVER) - bOrderMap.get(NAVER);
-                }
-
-                if (aOrderMap.containsKey(KAKAO)) {
-                    return -1;
-                } else {
-                    return 1;
-                }                
-            });
+            Collections.sort(mergedPlaces, this::comparePlace);
 
             CommonResponse ret = new CommonResponse();
             ret.setPlaces(mergedPlaces);
